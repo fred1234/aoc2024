c03bbffb3c8d55abee47a5a0ac049949c93e3c9d
Frederick Egli <frederick.egli@hslu.ch>
2024-12-02 10:12:32 +0100

    adding tests..

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -5,18 +5,24 @@ object Day01 {
     (splitted(0).toInt, splitted(1).toInt)
   }
 
+  def puzzleOne(l: List[Int], r: List[Int]): Int = {
+    l.sorted
+      .zip(r.sorted)
+      .map((l, r) => math.abs(l - r))
+      .sum
+  }
+
+  def puzzleTwo(l: List[Int], r: List[Int]): Int = {
+    val freq = r.groupBy(identity).mapValues(_.length)
+    l.map(value => value * freq.getOrElse(value, 0)).sum
+  }
+
   def main(args: Array[String]): Unit = {
     val raw: List[String] = os.read.lines(os.pwd / "src" / "main" / "resources" / "day01.txt").toList
-
     val (firstList: List[Int], secondList: List[Int]) = raw.unzip(parseLine)
 
-    val result = firstList.sorted().zip(secondList.sorted()).map((l, r) => math.abs(l - r)).sum
-    println(result)
-
-    val freq = secondList.groupBy(identity).mapValues(_.length)
-
-    val result2 = firstList.map(value => value * freq.getOrElse(value, 0)).sum
-    println(result2)
+    println(puzzleOne(firstList, secondList))
+    println(puzzleTwo(firstList, secondList))
 
   }
 }
