582ad4b81fcbb7abc7523dcb346ba94a5b1391ac
Frederick Egli <frederick.egli@hslu.ch>
2024-12-02 08:20:51 +0100

    using unzip

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,18 +1,21 @@
 object Day01 {
 
+  def parseLine(line: String): (Int, Int) = {
+    val splitted = line.split("   ")
+    (splitted(0).toInt, splitted(1).toInt)
+  }
+
   def main(args: Array[String]): Unit = {
-
     val raw: List[String] = os.read.lines(os.pwd / "src" / "main" / "resources" / "day01.txt").toList
-    val firstList = raw.map(line => line.split("   ").apply(0).toInt).sorted
-    val secondList = raw.map(line => line.split("   ").apply(1).toInt).sorted
 
-    // part1
-    val result = firstList.zip(secondList).map((l, r) => math.abs(l - r)).sum
+    val (firstList: List[Int], secondList: List[Int]) = raw.unzip(parseLine)
+
+    val result = firstList.sorted().zip(secondList.sorted()).map((l, r) => math.abs(l - r)).sum
     println(result)
 
-    // part2
-    val lookup = secondList.groupBy(identity).mapValues(_.length)
-    val result2 = firstList.map(value => value * lookup.getOrElse(value, 0)).sum
+    val freq = secondList.groupBy(identity).mapValues(_.length)
+
+    val result2 = firstList.map(value => value * freq.getOrElse(value, 0)).sum
     println(result2)
 
   }
