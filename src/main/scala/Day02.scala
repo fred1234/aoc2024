53a01dade6ea0d0b884110e6f37ce1de48192f1e
Frederick Egli <frederick.egli@hslu.ch>
2024-12-02 16:38:25 +0100

    day2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,45 +1,33 @@
 object Day02 {
 
-  def solution1(input: List[List[Int]]) = {
-    val differences = input.map { level =>
-      level
-        .sliding(2)
-        .toList
-        .map { case List(v1, v2) =>
-          v1 - v2
-        }
-    }
+  def parseLine(l: String): List[Int] = l.split(" ").map(_.toInt).toList
 
-    println(differences)
+  def pairs(level: List[Int]): List[(Int, Int)] = level.zip(level.tail)
+  def increases(input: List[(Int, Int)]): Boolean = input.forall(_ < _)
+  def decreases(input: List[(Int, Int)]): Boolean = input.forall(_ > _)
+  def inBounds(input: List[(Int, Int)]): Boolean = {
+    val diffs = input.map((v1, v2) => math.abs(v1 - v2))
+    (diffs.toSet -- Set(0, 1, 2, 3)).isEmpty
+  }
+  def safe(input: List[Int]): Boolean = {
+    val paired = pairs(input)
+    (increases(paired) || decreases(paired)) && inBounds(paired)
+  }
 
-    val passed = differences.map(diff => {
-      val allPositive = diff.forall(_ > 0)
-      val allNegative = diff.forall(_ < 0)
-
-      val allInRange = (diff.map(_.abs).toSet -- Set(0, 1, 2, 3)).size == 0
-
-      if ((allPositive || allNegative) && allInRange) 1 else 0
-
-    })
-
-    passed.sum
+  def tolerate(input: List[Int]): Boolean = {
+    input.indices.exists(index => safe(input.patch(index, Nil, 1)))
   }
 
   def main(args: Array[String]): Unit = {
 
-    val smallInput: List[List[Int]] = List(
-      List(7, 6, 4, 2, 1),
-      List(1, 2, 7, 8, 9),
-      List(9, 7, 6, 2, 1),
-      List(1, 3, 2, 4, 5),
-      List(8, 6, 4, 4, 1),
-      List(1, 3, 6, 7, 9)
-    )
-
     val raw: List[String] = os.read.lines(os.pwd / "src" / "main" / "resources" / "day02.txt").toList
-    val input: List[List[Int]] = raw.map(_.split(" ").map(_.toInt).toList)
+    val input = raw.map(parseLine)
 
-    println(solution1(smallInput))
+    val solution1 = input.count(safe)
+    println(solution1)
+
+    val solution2 = input.count(tolerate)
+    println(solution2)
 
   }
 }
---
+++ src/test/scala/Day02.scala
@@ -0,0 +1,78 @@
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+import Day02.parseLine
+import Day02.pairs
+import Day02.increases
+import Day02.decreases
+import Day02.inBounds
+import Day02.safe
+import Day02.tolerate
+
+class Day02Test extends munit.FunSuite {
+
+  val smallInput: List[List[Int]] = List(
+    List(7, 6, 4, 2, 1),
+    List(1, 2, 7, 8, 9),
+    List(9, 7, 6, 2, 1),
+    List(1, 3, 2, 4, 5),
+    List(8, 6, 4, 4, 1),
+    List(1, 3, 6, 7, 9)
+  )
+
+  test("one line of input should be parsed correctly") {
+    val obtained: List[Int] = parseLine("7 6 4 2 1")
+    val expected = List(7, 6, 4, 2, 1)
+    assertEquals(obtained, expected)
+  }
+
+  test("pairing a list works") {
+    val obtained = pairs(List(1, 2, 3))
+    val expected = List((1, 2), (2, 3))
+    assertEquals(obtained, expected)
+  }
+
+  test("checking for strict increasing") {
+    val inputOK = pairs(List(1, 2, 3))
+    assert(increases(inputOK))
+
+    val inputNOK = pairs(List(1, -1, 2, 3))
+    assert(!increases(inputNOK))
+
+  }
+
+  test("checking for strict decreasing") {
+    val inputOK = pairs(List(3, 2, 1))
+    assert(decreases(inputOK))
+
+    val inputNOK = pairs(List(1, -1, -2, 3))
+    assert(!decreases(inputNOK))
+
+  }
+
+  test("checking for inBound") {
+    val inputOK = pairs(List(1, 2, 4, 7))
+    assert(inBounds(inputOK))
+
+    val inputNOK = pairs(List(1, 2, 9, 7))
+    assert(!inBounds(inputNOK))
+
+  }
+
+  test("safety for small input") {
+    val obtained: List[Boolean] = smallInput.map(safe)
+
+    val expected = List(true, false, false, false, false, true)
+    assertEquals(obtained, expected)
+    assertEquals(2, smallInput.count(safe))
+
+  }
+  test("safety with tolerance for small input") {
+
+    val obtained: List[Boolean] = smallInput.map(tolerate)
+
+    val expected = List(true, false, false, true, true, true)
+    assertEquals(obtained, expected)
+    assertEquals(4, smallInput.count(tolerate))
+
+  }
+}
