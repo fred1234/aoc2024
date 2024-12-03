57d200726c97523b50b9f805e8414b7832e66e04
Frederick Egli <frederick.egli@hslu.ch>
2024-12-03 07:49:05 +0100

    added day03

---
+++ src/main/scala/Day03.scala
@@ -0,0 +1,43 @@
+import scala.compiletime.ops.boolean
+
+def solutionOne(input: String): Long = {
+  val re = """mul\((\d+{3}),(\d+{3})\)""".r
+
+  re.findAllIn(input)
+    .map {
+      case s"mul(${first},${last})" => first.toLong * last.toLong
+      case _                        => ???
+    }
+    .sum
+}
+
+def solutionTwo(input: String): Long = {
+  val re = """do\(\)|don't\(\)|mul\((\d+{3}),(\d+{3})\)""".r
+  case class Acc(enabled: Boolean = true, value: Long = 0L)
+
+  val result2: Acc = re
+    .findAllIn(input)
+    .foldLeft(z = Acc())(({
+      case (a @ Acc(true, v), s"mul(${first},${last})")  => a.copy(value = v + first.toLong * last.toLong)
+      case (a @ Acc(false, v), s"mul(${first},${last})") => a // case (a, _)
+      case (a, "do()")                                   => a.copy(enabled = true)
+      case (a, "don't()")                                => a.copy(enabled = false)
+      case _                                             => ???
+    }))
+  result2.value
+}
+
+object Day03 {
+
+  def main(args: Array[String]): Unit = {
+    val raw: String = os.read(os.pwd / "src" / "main" / "resources" / "day03.txt")
+
+    val result1 = solutionOne(raw)
+    println(result1)
+
+    val result2 = solutionTwo(raw)
+    println(result2)
+
+  }
+
+}
---
+++ src/test/scala/Day03.scala
@@ -0,0 +1,20 @@
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+
+class Day03Test extends munit.FunSuite {
+
+  val small_part_one = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+  val small_part_two = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
+
+  test("solution for part 1") {
+    val obtained: Long = solutionOne(small_part_one)
+    val expected = 161L
+    assertEquals(obtained, expected)
+  }
+  test("solution for part 2") {
+    val obtained: Long = solutionTwo(small_part_two)
+    val expected = 48L
+    assertEquals(obtained, expected)
+  }
+
+}
